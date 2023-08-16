18ac13f04650adce432bba5bb36fe8e10171caca
Naveen Kumar <mnkincit22@gmail.com>
2023-08-16 15:03:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Board from "./components/Board/Board";
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <Board />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Board.css";
+
+const Board = () => {
+  return (
+    <div className="board-container">
+      <div className="board">
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+        <div className="box">Hello</div>
+      </div>
+    </div>
+  );
+};
+
+export default Board;
